b6cd827ed4b4d7570e4403036afccd1fe2e02ff3
Brendon <brendonstahl97@gmail.com>
2023-06-15 15:49:13 -0400

    added isNot option to Game Phase Check

--- components/GamePhaseCheck/GamePhaseCheck.tsx
+++ components/GamePhaseCheck/GamePhaseCheck.tsx
@@ -1,18 +1,26 @@
 import { ReactElement } from "react";
 import { useAppContext } from "../context/AppContext";
+import { GAME_PHASE } from "@/lib/enums";
 
 interface IGamePhaseCheckProps {
   allowedPhases: GAME_PHASE[];
+  isNot?: boolean;
   children: ReactElement;
 }
 
 const GamePhaseCheck = ({
   allowedPhases = [],
   children,
+  isNot = false,
 }: IGamePhaseCheckProps) => {
   const { state } = useAppContext();
+  let condition = allowedPhases.includes(state.CurrentPhase);
 
-  if (allowedPhases.includes(state.CurrentPhase)) {
+  if (isNot) {
+    condition = !condition;
+  }
+
+  if (condition) {
     return <>{children}</>;
   } else {
     return <></>;
